24b7ea010198787d23057618042a711eab3cb68e
insertdevs <slqnted@gmail.com>
2024-01-22 19:37:16 -0500

    first commit

---
+++ public/js/load.jsx
@@ -0,0 +1,7 @@
+console.log("loading site, this could take a while...");
+const ver = "a0.1";
+function loadSite() {
+    const head = (
+        <title>Arc - {ver}</title>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,57 @@
+import express from "express";
+import { createServer } from "http";
+import { hostname } from "os";
+import { fileURLToPath } from "url";
+import { createBareServer } from '@tomphttp/bare-server-node';
+
+const publicPath = fileURLToPath(new URL("/public/", import.meta.url));
+
+const bare = createBareServer("/mathisfun/");
+const app = express();
+
+app.use(express.static(publicPath));
+
+const server = createServer((req, res) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+let port = parseInt(process.env.PORT || "");
+
+if (isNaN(port)) port = 8080;
+
+server.on("listening", () => {
+  const address = server.address();
+
+  console.log("Listening on:");
+  console.log(`\thttp://localhost:${address.port}`);
+  console.log(`\thttp://${hostname()}:${address.port}`);
+  console.log(
+    `\thttp://${
+      address.family === "IPv6" ? `[${address.address}]` : address.address
+    }:${address.port}`
+  );
+});
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+function shutdown() {
+  console.log("SIGTERM signal received: closing HTTP server");
+  server.close();
+  bare.close();
+  process.exit(0);
+}
+
+server.listen(port);
